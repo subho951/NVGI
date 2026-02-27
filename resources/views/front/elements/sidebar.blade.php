ef588675c6b20eab79c91d683be063fcc3f8a230
subhomoy samanta <subhomoy@keylines.net>
2026-02-28 00:01:53 +0530

    event data & ;2~image upload

--- resources/views/front/elements/sidebar.blade.php
+++ resources/views/front/elements/sidebar.blade.php
@@ -10,7 +10,7 @@ if(count($pageName) > 1){
 } else {
     $pageFunction  = '';
 }
-
+$role_id = session('user_data')['role_id'];
 ?>
 <style>
     .active-link {
@@ -20,88 +20,90 @@ if(count($pageName) > 1){
 
 <h4>Admin Panel 1.0</h4>
 <ul class="nav flex-column sidebar-menu">
-
+    <!-- Dashboard -->
     <li class="nav-item">
         <a href="<?= url('dashboard') ?>" class="nav-link <?= (($pageSegment == 'dashboard')?'active-link':'') ?>">
             <i class="fa fa-home"></i> <span>Dashboard</span>
         </a>
     </li>
     
-    <!-- Masters -->
-    <li class="nav-item">
-        <a class="nav-link d-flex justify-content-between align-items-center <?= in_array($pageSegment, ['unit','branch','subject','session','board','medium','know-about','class','religion']) ? 'active-link' : '' ?>"
-        data-bs-toggle="collapse"
-        href="#mastersMenu"
-        role="button"
-        aria-expanded="<?= in_array($pageSegment, ['unit','branch','subject','session','board','medium','know-about','class','religion']) ? 'true' : 'false' ?>"
-        aria-controls="mastersMenu">
-            <span><i class="fa-solid fa-database"></i> Masters</span>
-            <i class="fa-solid fa-angle-down menu-arrow"></i>
-        </a>
+    <?php if($role_id == 1){?>
+        <!-- Masters -->
+        <li class="nav-item">
+            <a class="nav-link d-flex justify-content-between align-items-center <?= in_array($pageSegment, ['unit','branch','subject','session','board','medium','know-about','class','religion']) ? 'active-link' : '' ?>"
+            data-bs-toggle="collapse"
+            href="#mastersMenu"
+            role="button"
+            aria-expanded="<?= in_array($pageSegment, ['unit','branch','subject','session','board','medium','know-about','class','religion']) ? 'true' : 'false' ?>"
+            aria-controls="mastersMenu">
+                <span><i class="fa-solid fa-database"></i> Masters</span>
+                <i class="fa-solid fa-angle-down menu-arrow"></i>
+            </a>
 
-        <ul class="collapse list-unstyled ps-3 <?= in_array($pageSegment, ['unit','branch','subject','session','board','medium','know-about','class','religion']) ? 'show' : '' ?>" id="mastersMenu">
-            <li>
-                <a href="<?= url('unit/list') ?>" class="nav-link <?= (($pageSegment == 'unit')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Units</span>
-                </a>
-            </li>
+            <ul class="collapse list-unstyled ps-3 <?= in_array($pageSegment, ['unit','branch','subject','session','board','medium','know-about','class','religion']) ? 'show' : '' ?>" id="mastersMenu">
+                <li>
+                    <a href="<?= url('unit/list') ?>" class="nav-link <?= (($pageSegment == 'unit')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Units</span>
+                    </a>
+                </li>
 
-            <li>
-                <a href="<?= url('branch/list') ?>" class="nav-link <?= (($pageSegment == 'branch')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Branches</span>
-                </a>
-            </li>
+                <li>
+                    <a href="<?= url('branch/list') ?>" class="nav-link <?= (($pageSegment == 'branch')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Branches</span>
+                    </a>
+                </li>
 
-            <li>
-                <a href="<?= url('subject/list') ?>" class="nav-link <?= (($pageSegment == 'subject')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Subjects</span>
-                </a>
-            </li>
+                <li>
+                    <a href="<?= url('subject/list') ?>" class="nav-link <?= (($pageSegment == 'subject')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Subjects</span>
+                    </a>
+                </li>
 
-            <li>
-                <a href="<?= url('session/list') ?>" class="nav-link <?= (($pageSegment == 'session')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Sessions</span>
-                </a>
-            </li>
+                <li>
+                    <a href="<?= url('session/list') ?>" class="nav-link <?= (($pageSegment == 'session')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Sessions</span>
+                    </a>
+                </li>
 
-            <li>
-                <a href="<?= url('board/list') ?>" class="nav-link <?= (($pageSegment == 'board')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Boards</span>
-                </a>
-            </li>
+                <li>
+                    <a href="<?= url('board/list') ?>" class="nav-link <?= (($pageSegment == 'board')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Boards</span>
+                    </a>
+                </li>
 
-            <li>
-                <a href="<?= url('medium/list') ?>" class="nav-link <?= (($pageSegment == 'medium')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Mediums</span>
-                </a>
-            </li>
+                <li>
+                    <a href="<?= url('medium/list') ?>" class="nav-link <?= (($pageSegment == 'medium')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Mediums</span>
+                    </a>
+                </li>
 
-            <li>
-                <a href="<?= url('religion/list') ?>" class="nav-link <?= (($pageSegment == 'religion')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Religion</span>
-                </a>
-            </li>
+                <li>
+                    <a href="<?= url('religion/list') ?>" class="nav-link <?= (($pageSegment == 'religion')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Religion</span>
+                    </a>
+                </li>
 
-            <!--<li>
-                <a href="<?= url('know-about/list') ?>" class="nav-link <?= (($pageSegment == 'know-about')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Know About Us</span>
-                </a>
-            </li>-->
+                <!--<li>
+                    <a href="<?= url('know-about/list') ?>" class="nav-link <?= (($pageSegment == 'know-about')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Know About Us</span>
+                    </a>
+                </li>-->
 
-            <li>
-                <a href="<?= url('class/list') ?>" class="nav-link <?= (($pageSegment == 'class')?'active-link':'') ?>">
-                    <i class="fa-solid fa-arrow-right"></i> <span>Class</span>
-                </a>
-            </li>
+                <li>
+                    <a href="<?= url('class/list') ?>" class="nav-link <?= (($pageSegment == 'class')?'active-link':'') ?>">
+                        <i class="fa-solid fa-arrow-right"></i> <span>Class</span>
+                    </a>
+                </li>
 
-        </ul>
-    </li>
+            </ul>
+        </li>
 
-    <li class="nav-item">
-        <a href="<?= url('front-desk/list') ?>" class="nav-link <?= (($pageSegment == 'front-desk')?'active-link':'') ?>">
-            <i class="fa-solid fa-bell-concierge"></i> <span>Front-Desks</span>
-        </a>
-    </li>
+        <li class="nav-item">
+            <a href="<?= url('front-desk/list') ?>" class="nav-link <?= (($pageSegment == 'front-desk')?'active-link':'') ?>">
+                <i class="fa-solid fa-bell-concierge"></i> <span>Front-Desks</span>
+            </a>
+        </li>
+    <?php }?>
 
     <!-- Students -->
     <li class="nav-item">
@@ -131,12 +133,14 @@ if(count($pageName) > 1){
         </ul>
     </li>    
 
-    <li class="nav-item">
-        <a href="<?= url('settings') ?>" class="nav-link <?= (($pageSegment == 'settings')?'active-link':'') ?>">
-            <i class="fa-solid fa-gears"></i> <span>Settings</span>
-        </a>
-    </li>
-
+    <?php if($role_id == 1){?>
+        <!-- Settings -->
+        <li class="nav-item">
+            <a href="<?= url('settings') ?>" class="nav-link <?= (($pageSegment == 'settings')?'active-link':'') ?>">
+                <i class="fa-solid fa-gears"></i> <span>Settings</span>
+            </a>
+        </li>
+    <?php }?>
 </ul>
 
 
