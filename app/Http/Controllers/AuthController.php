2493a31b30edb20ec9b884f70c773e3a54f8d737
subhomoy samanta <subhomoy@keylines.net>
2026-02-27 23:58:27 +0530

    event data & ;2~image upload

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,13 +62,13 @@ class AuthController extends Controller
                 // Store selected user info in session array
                 session([
                         'user_data' => [
-                        'user_id'       => $user->id,
-                        'name'          => $user->first_name . ' ' . $user->last_name,
-                        'email'         => $user->email,
-                        'phone'         => $user->phone,
-                        'role_id'       => $user->role_id,
-                        'is_user_login' => 1,
-                    ]
+                            'user_id'       => $user->id,
+                            'name'          => $user->first_name . ' ' . $user->last_name,
+                            'email'         => $user->email,
+                            'phone'         => $user->phone,
+                            'role_id'       => $user->role_id,
+                            'is_user_login' => 1,
+                        ]
                 ]);
 
                 // user activity
