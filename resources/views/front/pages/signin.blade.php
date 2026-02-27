5ee7cdcb9093d7748b90b7a6cdf7fc33bc457010
subhomoy samanta <subhomoy@keylines.net>
2026-02-27 23:38:16 +0530

    event data & ;2~image upload

--- resources/views/front/pages/signin.blade.php
+++ resources/views/front/pages/signin.blade.php
@@ -24,8 +24,8 @@ use App\Helpers\Helper;
 <form action="{{ url('signin') }}" method="POST">
     @csrf
     <div class="mb-3">
-        <label for="email" class="form-label">Username</label>
-        <input type="text" class="form-control" id="email" name="email" placeholder="Enter username" required>
+        <label for="email" class="form-label">Phone Number</label>
+        <input type="text" class="form-control" id="email" name="email" placeholder="Enter Phone Number" required>
     </div>
     <div class="mb-3">
         <label for="password" class="form-label">Password</label>
