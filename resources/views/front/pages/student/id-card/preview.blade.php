c314ce0e79c1ee4b81471aa7d2b22454b99044e7
subhomoy samanta <subhomoy@keylines.net>
2026-04-10 20:37:30 +0530

    id card gap decrease

--- resources/views/front/pages/student/id-card/preview.blade.php
+++ resources/views/front/pages/student/id-card/preview.blade.php
@@ -112,7 +112,7 @@ if ($cardBackgroundUrl === '') {
         .sheet-grid{
             display: grid;
             grid-template-columns: repeat(3, 85.6mm);
-            gap: 4mm;
+            gap: 2mm;
             justify-content: center;
             align-content: start;
             width: fit-content;
